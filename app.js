7aa6cb3140cd0abd6b3bb748aa3941fab1448071
alex <lapkesa@gmail.com>
2023-02-22 01:50:38 +0200

    check tests

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use((req, res, next) => {
 app.use('/', userRouter);
 app.use('/', cardRouter);
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'Ошибка на сервере' : message,
+  });
+  next();
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
