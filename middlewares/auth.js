3cc9e3c56958367e5e5069fc602bb4e0b1ee10a9
alex <lapkesa@gmail.com>
2023-03-11 02:19:59 +0200

    fix:fix error lint

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,24 +3,20 @@ const { SECRET_JWT } = require('../utils/constants');
 const { UnauthorizedError } = require('../utils/errors/UnauthorizedError');
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return UnauthorizedError(res);
   }
-
   const token = extractBearerToken(authorization);
   let payload;
-
   try {
     payload = jwt.verify(token, SECRET_JWT);
   } catch (err) {
     return UnauthorizedError(res);
   }
-
   req.user = payload; // записываем пейлоуд в объект запроса
-
   next(); // пропускаем запрос дальше
 };
