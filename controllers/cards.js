3cbed43abfac3b9c33b5aeb6b611cdfff28f5e3b
alex <lapkesa@gmail.com>
2023-03-11 12:33:43 +0200

    refactor: delete card

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,7 +30,7 @@ module.exports.deleteCard = (req, res, next) => {
       if (!card) {
         throw new NotFoundError(`Карточка с указанным id=${cardId} не найдена.`);
       }
-      if (card.owner !== req.user._id) {
+      if (card.owner !== cardId) {
         throw new ForbiddenError('Вы не можете удалить эту карточку, она чужая');
       }
       res.send({ message: 'Карточка удалена' });
