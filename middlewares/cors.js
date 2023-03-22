f36bf1aebf37e905695a5a26557f7f6764d91c4f
alex <lapkesa@gmail.com>
2023-03-23 01:33:50 +0200

    change cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,13 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'localhost:3000', 'https://localhost:3000'],
+  origin: [
+    'http://localhost:3000',
+    'localhost:3000',
+    'https://localhost:3000',
+    'http://mesto.lapkes.nomoredomains.work',
+    'https://mesto.lapkes.nomoredomains.work',
+  ],
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
   credentials: true,
   optionsSuccessStatus: 200,
