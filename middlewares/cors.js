efbea44ff2d9770b3c81236702e7f492e5e0a036
alex <lapkesa@gmail.com>
2023-03-23 11:45:12 +0200

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,3 +1,7 @@
+const cors = require('cors');
+
+const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE,HEAD';
+
 const allowedCors = [
   'http://localhost:3000',
   'localhost:3000',
@@ -5,20 +9,12 @@ const allowedCors = [
   'http://mesto.lapkes.nomoredomains.work',
   'https://mesto.lapkes.nomoredomains.work',
 ];
-const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE,HEAD';
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const { method } = req;
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
+
+const corsOptions = {
+  origin: allowedCors,
+  credentials: true,
+  methods: DEFAULT_ALLOWED_METHODS,
+  allowedHeaders: 'Content-Type,Authorization',
 };
+
+module.exports = cors(corsOptions);
