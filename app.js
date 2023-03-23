9f3708189d9e71134df8ee8c5e37c3b8fa2f8f3d
alex <lapkesa@gmail.com>
2023-03-23 21:52:47 +0200

    fix

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const rootRouter = require('./routes/index');
+const auth = require('./middlewares/auth');
+const { cors } = require('./middlewares/cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+
+const { PORT = 3000, DB_ADDRESS = 'mongodb://0.0.0.0:27017/mestodb' } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(requestLogger);
+app.use(helmet());
+mongoose
+  .connect(DB_ADDRESS)
+  .then(() => console.log('Соединение с базой данных установлено'))
+  .catch((err) => {
+    console.log(`Ошибка при подключении к базе данных: ${err.message}`);
+  });
+
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(auth);
+app.use(rootRouter);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
