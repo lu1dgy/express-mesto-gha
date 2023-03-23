af8c15a57d87584d5152b3f44c8adf23a6856f6d
alex <lapkesa@gmail.com>
2023-03-23 02:03:18 +0200

    refactor:change cors settings to settings from training

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,17 +1,24 @@
-const cors = require('cors');
-
-const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'localhost:3000',
-    'https://localhost:3000',
-    'http://mesto.lapkes.nomoredomains.work',
-    'https://mesto.lapkes.nomoredomains.work',
-    'mesto.lapkes.nomoredomains.work',
-  ],
-  methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-  credentials: true,
-  optionsSuccessStatus: 200,
+const allowedCors = [
+  'http://localhost:3000',
+  'localhost:3000',
+  'https://localhost:3000',
+  'http://mesto.lapkes.nomoredomains.work',
+  'https://mesto.lapkes.nomoredomains.work',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE';
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
 };
-
-module.exports = cors(corsOptions);
