e3c46cb03347b2d8eb7e288d04f7669700982e6e
alex <lapkesa@gmail.com>
2023-03-23 11:17:27 +0200

    add dotenv conf

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,3 @@
-module.exports.SECRET_JWT = 'asdasd2124121hv1jh412jh412ghj412fjh412f';
+const { SECRET_JWT } = process.env;
+module.exports.SECRET_JWT = SECRET_JWT;
 module.exports.AVATAR_URL_REGEX = /^(http|https):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i;
